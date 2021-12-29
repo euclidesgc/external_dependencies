47aa9a053e09d27240d8cc240db7f2d7dcabe51c
Euclides Catunda <euclides.catunda@b2wdigital.com>
2021-12-29 13:18:39 -0300

    correção do pubspec utilizando git ao invés de path

--- lib/external_dependencies.dart
+++ lib/external_dependencies.dart
@@ -1,3 +1,3 @@
-export 'package:flutter_modular/flutter_modular.dart' hide RouterOutlet, CustomTransition;
-export 'package:dio/dio.dart' hide FormData, Response, MultipartFile;
-export 'package:get/get.dart';
+export 'package:flutter_modular/flutter_modular.dart';
+export 'package:dio/dio.dart';
+
